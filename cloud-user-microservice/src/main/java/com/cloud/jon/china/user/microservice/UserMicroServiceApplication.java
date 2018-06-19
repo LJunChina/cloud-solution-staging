58d9d28889e5124abc0dd74d04c8cb236280f528
Jon_China <503665965@qq.com>
2018-06-19 10:27:17 +0800

    feat(user-microservice):用户系统初始化

---
+++ cloud-user-microservice/src/main/java/com/cloud/jon/china/user/microservice/UserMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.jon.china.user.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroServiceApplication.class, args);
+	}
+}
---
+++ cloud-user-microservice/src/test/java/com/cloud/jon/china/user/microservice/UserMicroServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.jon.china.user.microservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserMicroServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
