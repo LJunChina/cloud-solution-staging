ab311573619cd2434a0926f5fb861146972b153e
Jon_China <503665965@qq.com>
2018-06-26 09:10:57 +0800

    feat(eureka):默认不使用高可用

--- cloud-register-center/src/main/java/com/cloud/jon/china/register/center/CloudRegisterCenterApplication.java
+++ cloud-register-center/src/main/java/com/cloud/jon/china/register/center/CloudRegisterCenterApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.Objects;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class CloudRegisterCenterApplication {
 
 	public static void main(String[] args) {
+		if(Objects.isNull(System.getProperty("spring.profiles.active"))){
+			System.setProperty("spring.profiles.active","dev");
+		}
 		SpringApplication.run(CloudRegisterCenterApplication.class, args);
 	}
 }
