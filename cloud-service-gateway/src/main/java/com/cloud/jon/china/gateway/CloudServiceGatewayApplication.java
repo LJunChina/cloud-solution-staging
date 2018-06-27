6bfe8526824b2c3d3ce6d657e97c7823d0690b37
Jon_China <503665965@qq.com>
2018-06-27 17:01:28 +0800

    feat(gateway):gateway配置

--- cloud-service-gateway/src/main/java/com/cloud/jon/china/gateway/CloudServiceGatewayApplication.java
+++ cloud-service-gateway/src/main/java/com/cloud/jon/china/gateway/CloudServiceGatewayApplication.java
@@ -1,8 +1,23 @@
 package com.cloud.jon.china.gateway;
 
+import com.cloud.common.util.EmptyChecker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * @author Jon_China
  * @create 2018/6/27
  */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class CloudServiceGatewayApplication {
+    public static void main(String[] args) {
+        if(EmptyChecker.isEmpty(System.getProperty("spring.profiles.active"))){
+            System.setProperty("spring.profiles.active","dev");
+        }
+        SpringApplication.run(CloudServiceGatewayApplication.class, args);
+    }
 }
